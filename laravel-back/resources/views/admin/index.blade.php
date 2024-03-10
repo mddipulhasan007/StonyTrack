0b510b7c86d8ec5afaac558bab09e9d30f993092
dipul@429820 <mddipulhasan@gmail.com>
2024-03-10 11:14:26 +0600

    update

--- laravel-back/resources/views/admin/index.blade.php
+++
@@ -1,5 +0,0 @@
-@include('layout.admin.header')
-
-    @include('admin.dashboard')
-      
-@include('layout.admin.footer')
--- laravel-back/resources/views/frontend/index.blade.php
+++
@@ -1,5 +0,0 @@
-@include('layout.frontend.header')
-
-    @include('frontend.home')
-      
-@include('layout.frontend.footer')
